e030bb01ceecd88c5cd33952bc901a9daeaba589
Odilon Relucio <odilon@intelimina.com>
2025-06-17 23:43:32 +0800

    Minor changes

--- frappe/www/login.py
+++ frappe/www/login.py
@@ -147,7 +147,7 @@ def send_login_link(email: str):
 		frappe.get_website_settings("app_name") or frappe.get_system_settings("app_name") or _("Frappe")
 	)
 
-	subject = _("Login To {0}").format(app_name)
+	subject = "Login to Tech Navy"
 
 	frappe.sendmail(
 		subject=subject,
